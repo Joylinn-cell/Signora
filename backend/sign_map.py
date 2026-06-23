064cb169b5c993ddd4c99857d2aba938cd0ce3d5
darcymatt <darcymathew@gmail.com>
2026-06-23 16:08:30 +0530

    added translation and speech modules

---
+++ backend/sign_map.py
@@ -0,0 +1,16 @@
+SIGN_MAP = {
+
+    "DOCTOR": "I need a doctor",
+
+    "HELP": "Please help me",
+
+    "WATER": "I need water",
+
+    "MEDICINE": "I need medicine",
+
+    "PAIN": "I am in pain",
+
+    "FOOD": "I need food",
+
+    "EMERGENCY": "This is an emergency"
+}
